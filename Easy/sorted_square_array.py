a821112b01fae372c4a27c3406211f5f12b5adfa
schmittydog <sdunn@corevera.com>
2026-02-27 21:46:26 -0800

    tournament_winner

--- Easy/sorted_square_array.py
+++ Easy/sorted_square_array.py
@@ -8,4 +8,19 @@ def sortedSquaredArray(array):
     array.sort(key=abs)
     return [n**2 for n in array]
 
-
+def sortedSquaredArray(array):
+    '''
+    Time: N
+    Space: N
+    '''
+    l, r = 0, len(array) - 1
+    squares = []
+    while r >= l:
+        if abs(array[r]) >= abs(array[l]):
+            squares.append(array[r]**2)
+            r -= 1
+        else:
+            squares.append(array[l]**2)
+            l += 1
+    squares.reverse()
+    return squares
