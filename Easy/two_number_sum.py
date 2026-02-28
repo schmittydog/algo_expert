3e15960aed9e37963a0c07a0117467e99a17d2d6
schmittydog <sdunn@corevera.com>
2026-02-27 20:07:31 -0800

    2 more solutions to two_number_sum

--- Easy/two_number_sum.py
+++ Easy/two_number_sum.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 
 def two_number_sum(array, target_sum):
-    target_numbers = []
+    '''
+        O(N) Time
+        O(N) Space
+    '''
     numbers_visited = set()
     for number in array:
         if target_sum - number in numbers_visited:
             target_numbers = [target_sum - number, number]
-            break
         numbers_visited.add(number)
-    return target_numbers
+    return []
+
+def twoNumberSum(array, target_sum):
+    ''' 
+        O(Nlog(N)) Time
+        O(1) Space
+    '''
+    array.sort()
+    l, r = 0, len(array) - 1
+    while l < r:
+        s = array[l] + array[r]
+        if s == target_sum:
+            return [array[l], array[r]]
+        elif s < target_sum:
+            l += 1
+        else:
+            r -= 1
+    return []
+
+def twoNumberSum(array, target_sum):
+    ''' 
+        O(N**2) Time
+        O(1) Space
+    '''
+    l = len(array)
+    for i in range(l-1):
+        for j in range(i+1, l):
+            if array[i] + array[j] == target_sum:
+                return [array[i], array[j]]
+    return []
