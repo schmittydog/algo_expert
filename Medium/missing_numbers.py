949da68bb01a9c076038a0193f8710110b2c54ca
schmittydog <sdunn@corevera.com>
2026-03-05 08:50:39 -0800

    added continuous_median

--- Medium/missing_numbers.py
+++ Medium/missing_numbers.py
@@ -9,5 +9,24 @@ def missingNumbers(nums):
     print(s)
     return [n for n in range(1, len(nums+3)) if n not in s]
 
+def missingNumbers(nums):
+    '''
+        Time: N
+        Space: 1
+    '''
+    n = len(nums) + 2
+    full_sum = (n*n+n)//2
+    num_sum = sum(nums)
+    diff = full_sum - num_sum
+    sum_of_diffs = 0
+    for n1 in range(diff-n, diff//2+1):
+        sum_of_diffs += diff - 2*n1
+    print(diff, sum_of_diffs)
+    for num in nums:
+        if num >= diff-n and num <= diff//2:
+            sum_of_diffs += 2*n1 - diff
+    n1 = (diff-sum_of_diffs)//2
+    n2 = n1 + sum_of_diffs
+    return sorted([n1,n2])
 
 print(missingNumbers([1,3,4]))
--- Medium/staircase_traversal.py
+++ Medium/staircase_traversal.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+from queue import deque
+def staircaseTraversal(height, steps):
+    '''
+        Time: height
+        Space: steps
+        Track with queue
+    '''
+    count = 1
+    q = deque((steps-1)*[0] + [1])
+    for n in range(1, height+1):
+        q.append(count)
+        count *= 2
+        count -= q.popleft()
+    return q[-1]
+
+
 def staircaseTraversal(height, maxSteps):
     '''
         Time: height * maxSteps
