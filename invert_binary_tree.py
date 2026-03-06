a8cc31e35e6c489b5db761e180d95e3642f08dc2
schmittydog <sdunn@corevera.com>
2026-03-05 21:06:20 -0800

    added invert_binary_tree.py

---
+++ invert_binary_tree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def invertBinaryTree(tree):
+    if tree is None:
+        return
+    tree.left, tree.right = tree.right, tree.left
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
