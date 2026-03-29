75f38b872956dd9435543cb5c6f4d876fbc4de4f
schmittydog <sdunn@corevera.com>
2026-03-29 08:03:28 -0700

    cleaned up laptop_rentals.py

--- laptop_rentals.py
+++ laptop_rentals.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-
 from heapq import heappop, heappush
 
 def laptopRentals(times):
-    if not times:
-        return 0
     times.sort()
-    q = [times[0][1]]
-    for idx in range(1, len(times)):
-        start, end = times[idx]
-        if start < q[0]:
-            heappush(q, end)
-        else:
+    q = []
+    for start, end in times:
+        if q and start >= q[0]:
             heappop(q)
-            heappush(q, end)
+        heappush(q, end)
     return len(q)
