8135e123756b9c68c730f23cd8209b1f8e54ea7a
schmittydog <sdunn@corevera.com>
2026-04-10 13:46:41 -0700

    made it O(1)

--- longest_peaks.py
+++ longest_peaks.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
 def longestPeak(array):
-    peaks = []
+    longest_peak, i = 0, 1
     for i in range(1, len(array) - 1):
-        if array[i-1] < array[i] and array[i+1] < array[i]:
-            peaks.append(i)
-    longest_peak = 0
-    for peak in peaks:
-        start, end = peak - 1, peak + 1
-        while start > 0 and array[start] > array[start-1]:
-            start -= 1
-        while end < len(array) - 1 and array[end] > array[end+1]:
-            end += 1
-        longest_peak = max(longest_peak, end - start + 1)
+        if array[i] > array[i-1] and array[i] > array[i+1]:
+            start, end = i - 1, i + 1
+            while start > 0 and array[start] > array[start-1]:
+                start -= 1
+            while end < len(array) - 1 and array[end] > array[end+1]:
+                end += 1
+            longest_peak = max(longest_peak, end - start + 1)
     return longest_peak
-
