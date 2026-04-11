99e97113937be9b314c7164c80bfabb061668237
schmittydog <sdunn@corevera.com>
2026-04-10 18:17:25 -0700

    added N^2

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -9,3 +9,22 @@ def longestPalindromicSubstring(string):
                 if word == word[::-1]:
                     longest = word
     return longest
+
+# N^2
+def longestPalindromicSubstring(string):
+    longest = ''
+    for idx in range(len(string)):
+        # check even
+        left, right = idx, idx-1
+        while left > 0 and right < len(string) - 1 and string[left-1] == string[right+1]:
+            left, right = left - 1, right + 1
+        if right - left + 1 > len(longest):
+            longest = string[left:right+1]
+        # Check odd
+        left, right = idx, idx
+        while left > 0 and right < len(string) - 1 and string[left-1] == string[right+1]:
+            left, right = left - 1, right + 1
+        if right - left + 1 > len(longest):
+            longest = string[left:right+1]
+    return longest
+
